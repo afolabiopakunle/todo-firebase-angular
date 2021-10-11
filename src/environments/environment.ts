f813680585804a9b08a2f048786b629a23c77930
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-10-11 11:14:14 +0100

    firebase added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDvXv2CZ91XUTzrZxtRxZaAiOHi_Qn4s6o',
+    authDomain: 'todo-firebase-angular.firebaseapp.com',
+    projectId: 'todo-firebase-angular',
+    storageBucket: 'todo-firebase-angular.appspot.com',
+    messagingSenderId: '1023950658007',
+    appId: '1:1023950658007:web:73c4487bbf4a1bde925392'
+  }
 };
 
 /*
